dc03d46bfda8af1669623c16a551a8107ec5357c
ghostyii <ghostyiigame@outlook.com>
2017-03-13 00:24:07 +0800

    Mine Sweeper
    Complete core functions

---
+++ Assets/Scripts/Element.cs
@@ -0,0 +1,64 @@
+﻿//ORG: Ghostyii & MoonLight Game
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Element : MonoBehaviour
+{
+    public bool isMine = false;
+    public Sprite[] emptySprites;
+    public Sprite[] mineSprites;
+    //public 
+
+    private Button button = null;
+    private Image image = null;
+
+    private int girdX = 0, girdY = 0;    
+
+    private void Start()
+    {
+        isMine = Random.value < 0.1f;
+        button = GetComponent<Button>();
+        image = GetComponent<Image>();
+
+        girdX = (int)((image.rectTransform.anchoredPosition.x - 10) / 20);
+        girdY = (int)((image.rectTransform.anchoredPosition.y + 10) / -20);
+
+        Gird.elements[girdX, girdY] = this;
+    }
+
+    public void UncoverMine(bool isClick)
+    {
+        if (image == null) image = GetComponent<Image>();
+
+        image.sprite = isClick ? mineSprites[0] : mineSprites[1];
+
+        button.enabled = false;
+    }
+
+    public void UncoverNormal(int mineCount)
+    {
+        if (image == null) image = GetComponent<Image>();
+
+        image.sprite = emptySprites[mineCount];
+
+        button.enabled = false;
+    }
+
+    public void ElementOnClick()
+    {
+        if (image == null) image = GetComponent<Image>();
+
+        if (isMine)
+        {
+            Gird.UncoverAllMines(this.GetInstanceID());                   
+        }
+        else
+        {
+            Gird.FFUncover(girdX, girdY, new bool[Gird.width, Gird.height]);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Gird.cs
@@ -0,0 +1,63 @@
+﻿//ORG: Ghostyii & MoonLight Game
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gird
+{
+    static public int width = 16;
+    static public int height = 16;
+
+    static public Element[,] elements = new Element[width, height];
+
+    static public bool MineAt(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+            return elements[x, y].isMine;
+
+        return false;
+    }
+
+    static public int GetMineCount(int x, int y)
+    {
+        int count = 0;
+
+        if (MineAt(x - 1, y - 1)) count++;
+        if (MineAt(x, y - 1)) count++;
+        if (MineAt(x + 1, y + 1)) count++;
+        if (MineAt(x - 1, y)) count++;
+        if (MineAt(x + 1, y)) count++;
+        if (MineAt(x - 1, y + 1)) count++;
+        if (MineAt(x, y + 1)) count++;
+        if (MineAt(x + 1, y + 1)) count++;
+
+        return count;
+    }
+
+    static public void UncoverAllMines(int clickID)
+    {
+        foreach (var item in elements)
+            if (item.isMine)
+                item.UncoverMine(clickID == item.GetInstanceID());
+    }
+
+    static public void FFUncover(int x,int y,bool[,] visted)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            if (visted[x, y]) return;
+
+            int count = GetMineCount(x, y);
+            elements[x, y].UncoverNormal(count);            
+            if (count > 0) return;
+
+            visted[x, y] = true;
+
+            FFUncover(x, y - 1, visted);
+            FFUncover(x - 1, y, visted);
+            FFUncover(x + 1, y, visted);
+            FFUncover(x, y + 1, visted);            
+        }
+    }
+}
+
